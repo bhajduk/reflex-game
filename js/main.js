50cc82d8bda8d405fdb9c25bf4bb2df8be62d70c
bhajduk <unival12@gmail.com>
2021-03-14 14:22:36 +0100

    feature done

--- js/main.js
+++ js/main.js
@@ -3,56 +3,64 @@ let i = 10;
 let gameBoardWidth = document.querySelector('#gameMain').clientWidth;
 let gameBoardHeight = document.querySelector('#gameMain').clientHeight;
 
+//fit game area when resizing
 window.addEventListener('resize', () => {
     gameBoardWidth = document.querySelector('#gameMain').clientWidth;
     gameBoardHeight = document.querySelector('#gameMain').clientHeight;
 });
 
-function getRandomIntInclusive(min, max) {
+const getRandomIntInclusive = (min, max) => {
     min = Math.ceil(min);
     max = Math.floor(max);
-    console.log(gameBoardWidth);
-    console.log(gameBoardHeight);
+    // console.log(gameBoardWidth);
+    // console.log(gameBoardHeight);
     return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
 
-function backgroundHit() {
+const backgroundHit = () => {
     counter--;
-    document.getElementById('gameMain').style.backgroundColor = 'red';
-}
+    document.querySelector('#gameMain').style.backgroundColor =
+        'rgb(100, 150, 150)';
+};
 
-function randomShow() {
-    let x = getRandomIntInclusive(-gameBoardWidth / 2, gameBoardWidth / 2);
-    let y = getRandomIntInclusive(-gameBoardHeight / 2, gameBoardHeight / 2);
+const randomShow = () => {
+    let x = getRandomIntInclusive(
+        -gameBoardWidth / 2 + 70,
+        gameBoardWidth / 2 - 70
+    );
+    let y = getRandomIntInclusive(
+        -gameBoardHeight / 2 + 70,
+        gameBoardHeight / 2 - 70
+    );
 
-    if (i < 3) document.getElementById('circle').style.borderColor = 'red';
+    if (i < 3) document.querySelector('#circle').style.borderColor = 'red';
 
-    document.getElementById('gameMain').style.backgroundColor =
-        'rgb(151, 190, 190);';
+    document.querySelector('#circle').innerHTML = '';
 
-    document.getElementById('circle').innerHTML = '';
-
-    document.getElementById('circle').style.transform =
+    document.querySelector('#circle').style.transform =
         'translate(' + x + 'px,' + y + 'px)';
     counter++;
-}
 
-function timer() {
-    document.getElementById('time').innerHTML = `time: ${i}`;
+    document.querySelector('#gameMain').style.backgroundColor =
+        'rgb(151, 190, 190)';
+    console.log('klik');
+};
+
+const timer = () => {
+    document.querySelector('#time').innerHTML = `time: ${i}`;
     i--;
 
-    if (i < 3) document.getElementById('time').style.borderColor = 'red';
+    if (i < 3) document.querySelector('.sum#time').style.color = 'red';
 
     if (i < -1) {
-        document.getElementById('personalBest').innerHTML = 'score: ' + counter;
-
-        document.getElementById('circle').setAttribute('onclick', ';');
-        document.getElementById('circle').style.cursor = 'default';
-        document.getElementById('gameMain').setAttribute('onclick', ';');
-        document.getElementById('time').innerHTML = 'time expired';
-        document.getElementById('gameMain').innerHTML =
-            '<div id = "retry" onclick = "location.reload();">try again</div>';
-        document.getElementById('gameMain').style.backgroundColor =
-            'rgb(23, 23, 23)';
+        document.querySelector('#personalBest').innerHTML = 'score: ' + counter;
+        document.querySelector('#circle').setAttribute('onclick', ';');
+        document.querySelector('#circle').style.cursor = 'default';
+        document.querySelector('#gameMain').setAttribute('onclick', ';');
+        document.querySelector('#time').innerHTML = 'time expired';
+        document.querySelector('#gameMain').innerHTML =
+            '<div id = "retry" style = "display: flex; justify-content: center; align-items: center;" onclick = "location.reload();">try again</div>';
+        document.querySelector('#gameMain').style.backgroundColor =
+            'rgb(100, 150, 150)';
     } else setTimeout(timer, 1000);
-}
+};
